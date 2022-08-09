7a6861c94883cddc24aa3383292ade5a0b629f73
Ziermaier Maximilian <max@ziermaier.de>
2022-08-09 12:13:05 +0200

    Initial upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$address = 'http://192.168.178.123';
+$user = 'installer';
+$password = 'XXXXXXXXXXXX';
+
+
+$frommail = "solarlog@domain.tld";
+$fromname = "Solarlog";
+$recvaddress = "receiver@domain.tld";
+$recvadrname = "Receiver";
+
+# for use with https://github.com/PHPMailer/PHPMailer/
+# tested with v6.6.3
+$mailhost = "192.168.178.234";
+$SMTPAuth = false;
+$mailusername = "";
+$mailpassword = "";
+$SMTPDebug = 0;
